b44a1d7c4dea17f828738c51282825df46f13690
Kozlov Yakov <yk@exante.eu>
2015-12-17 19:50:29 +0300

    Default values for optional fields

--- src/emodel.erl
+++ src/emodel.erl
@@ -17,10 +17,15 @@
 
 -type opts() :: #{}.
 
--type req_opts() :: required | optional.
--type position() :: non_neg_integer() | atom() | binary().
--type req_fun(Model) :: fun((Model) -> req_opts()).
--type default_fun(A, Model, R) :: fun((Model) -> {ok, A} | {error, R}).
+-type req_opts() :: required | optional | ignore.
+-type req_fun(M) :: fun((M) -> req_opts()).
+
+-type required(M) :: req_opts() | req_fun(M).
+
+-type position() :: non_neg_integer() | %% For tuple models
+                    term(). %% form maps models
+
+-type default(A, M, R) :: fun((M) -> {ok, A} | {error, R}).
 -type model_type() :: tuple | map.
 
 -type field() ::
@@ -29,29 +34,29 @@
               req_fun(Model),
               Getter :: fun((Model) -> Value),
               Setter :: fun((Value, Model) -> {ok, Model} | {error, Reason :: any()}),
-              Default :: default_fun(Value, Model, Reason :: any())
+              default(Value, Model, Reason :: any())
           }.
 
 -opaque model() :: {'$compiled', [field()]}.
 -type pre_model() ::
           [{
                Name :: binary(),
-               req_opts() | req_fun(Model),
+               required(M),
                Type :: emodel_converters:known_types(A :: any(), B, Reason) | any(),
                Position :: position(),
                Validators :: [emodel_validators:validator(B, Model, Reason :: any()) | any()]
            } |
            {
                Name :: binary(),
-               req_opts() | req_fun(Model),
+               required(M),
                Type :: emodel_converters:known_types(A :: any(), B, Reason) | any(),
                Position :: position(),
                Validators :: [emodel_validators:validator(B, Model, Reason :: any()) | any()],
-               Default :: any()
+               default(B, M, Reason :: any()) | B
            } |
            {
                Name :: binary(),
-               req_opts() | req_fun(Model),
+               required(M),
                Setter :: fun((any(), Model) -> {ok, Model} | {error, Reason :: any()})
            }].
 
@@ -61,6 +66,8 @@
         converters => fun emodel_converters:get_converter/2,
         validators => fun emodel_validators:get_validator/2
     }).
+
+-define(IS_UNDEFINED(V), (V =:= undefined orelse V =:= null)).
 
 %% =============================================================================
 %% Default versions
@@ -130,7 +137,7 @@ from_map(Map, Model, ModelDescription0, Opts) when is_map(Map) ->
                 Required ->
                     case maps:find(Name, Map) of
                         {ok, null} when Required =:= required ->
-                            {error, {Name, <<"required">>}};
+                            {error, {Name, required}};
                         {ok, Value} ->
                             case Setter(Value, M) of
                                 {ok, _Model} = Ok -> Ok;
@@ -139,10 +146,7 @@ from_map(Map, Model, ModelDescription0, Opts) when is_map(Map) ->
                         error ->
                             case Getter(M) of
                                 undefined ->
-                                    case Required of
-                                        optional -> ok;
-                                        required -> try_default(Default, Setter, Name, M)
-                                    end;
+                                    try_default(Required, Default, Setter, Name, M);
                                 _ -> {ok, M}
                             end
                     end
@@ -155,10 +159,14 @@ from_map(_, _, _, _) ->
 %%% Internal functions
 %% =============================================================================
 
-try_default(undefined, _Setter, Name, _Model) ->
-    {error, {Name, <<"required">>}};
-try_default(Default, Setter, Name, Model) ->
+try_default(required, undefined, _Setter, Name, _Model) ->
+    {error, {Name, required}};
+try_default(_, undefined, _Setter, _Name, Model) ->
+    {ok, Model};
+try_default(Required, Default, Setter, Name, Model) ->
     case Default(Model) of
+        {ok, Value} when ?IS_UNDEFINED(Value) andalso Required =:= required ->
+            {error, {Name, required}};
         {ok, Value} ->
             case Setter(Value, Model) of
                 {ok, _Model} = Ok -> Ok;
@@ -189,11 +197,14 @@ default_getter(tuple, Position) ->
 default_setter(ModelType, Converter, Position, Validators) ->
     Setter =
         case ModelType of
-            map ->   fun(V, M) -> maps:put(Position, V, M) end;
+            map ->
+                fun(undefined, M) -> maps:remove(Position, M);
+                   (V, M) -> maps:put(Position, V, M)
+                end;
             tuple -> fun(V, T) -> setelement(Position, T, V) end
         end,
-    fun(null, Model) ->
-           {ok, Setter(undefined, Model)};
+    fun(Value, Model) when ?IS_UNDEFINED(Value) ->
+           {ok, Setter(Value, Model)};
        (Data, Model) ->
            case Converter(Data) of
                {ok, ConvertedData} ->
@@ -209,5 +220,72 @@ default_setter(ModelType, Converter, Position, Validators) ->
 %%% Utils
 %% =============================================================================
 
--spec default_value(V) -> fun((any()) -> {ok, V} | {error, Reason :: any()}).
-default_value(Value) -> fun(null) -> {error, Value}; (_Model) -> {ok, Value} end.
+-spec default_value(V) -> fun((any()) -> {ok, V}).
+default_value(Value) -> fun(_Model) -> {ok, Value} end.
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+-record(r, {i,s,d}).
+
+simple_test_() ->
+    RExpected = #r{i=1, s = <<"a">>, d = {2015,12,01}},
+    MExpected = #{i => 1, s => <<"a">>, d => {2015,12,01}},
+    Data = #{
+        <<"int">> => 1,
+        <<"str">> => <<"a">>,
+        <<"date">> => <<"2015-12-01">>
+    },
+    [
+     ?_assertEqual(
+          {ok, RExpected},
+          emodel:from_map(Data, #r{}, [
+              {<<"int">>,  required, integer, #r.i, []},
+              {<<"str">>,  required, string, #r.s, []},
+              {<<"date">>, required, date, #r.d, []}
+          ])),
+     ?_assertEqual(
+          {ok, MExpected},
+          emodel:from_map(Data, #{}, [
+              {<<"int">>,  required, integer, i, []},
+              {<<"str">>,  required, string, s, []},
+              {<<"date">>, required, date, d, []}
+          ]))
+    ].
+
+default_test_() ->
+    [
+     ?_assertEqual(
+          {ok, #{ d => 1 }},
+          emodel:from_map(#{}, #{}, [
+              {<<"d">>, required, integer, d, [], emodel:default_value(1)}
+          ])),
+     ?_assertEqual(
+          {error, [{<<"d">>, required}]},
+          emodel:from_map(#{}, #{}, [
+              {<<"d">>, required, integer, d, [], emodel:default_value(null)}
+          ])),
+     ?_assertEqual(
+          {error, [{<<"d">>, required}]},
+          emodel:from_map(#{}, #{}, [
+              {<<"d">>, required, integer, d, [], emodel:default_value(undefined)}
+          ])),
+     %% Optional
+     ?_assertEqual(
+          {ok, #{ d => 1 }},
+          emodel:from_map(#{}, #{}, [
+              {<<"d">>, optional, integer, d, [], emodel:default_value(1)}
+          ])),
+     ?_assertEqual(
+          {ok, #{ d => null }},
+          emodel:from_map(#{}, #{}, [
+              {<<"d">>, optional, integer, d, [], emodel:default_value(null)}
+          ])),
+     ?_assertEqual(
+          {ok, #{}},
+          emodel:from_map(#{}, #{}, [
+              {<<"d">>, optional, integer, d, [], emodel:default_value(undefined)}
+          ]))
+    ].
+
+-endif.
